93170da32a11ac4841d34874bc7294d92ae8c5d3
fuaberu <86504230+fuaberu@users.noreply.github.com>
2024-03-05 09:20:46 -0300

    Fix button IOS

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -52,7 +52,7 @@ export const Hero = () => {
 
 				<Link
 					href="/contact"
-					className="relative inline-flex overflow-hidden rounded-full p-1 text-xl font-bold text-black focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 dark:text-white md:text-3xl"
+					className="relative isolate inline-flex overflow-hidden rounded-full p-1 text-xl font-bold text-black focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 dark:text-white md:text-3xl"
 				>
 					<span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
 					<span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-background px-4 py-2 backdrop-blur-3xl">
