a39da18825afc4746aa1003bf4b14da56b7f4ad5
fuaberu <86504230+fuaberu@users.noreply.github.com>
2024-03-05 10:53:32 -0300

    Fix resume link

--- src/components/ui/resume-link.tsx
+++ src/components/ui/resume-link.tsx
@@ -7,14 +7,14 @@ export const ResumeLink = () => {
 	return (
 		<div className="group relative block self-stretch">
 			<div className="absolute inset-0 -z-10 rounded-md bg-teal-300"></div>
-			<Link
+			<a
 				href="/resume.pdf"
 				target="_blank"
 				className="flex h-full flex-col items-center justify-center rounded-md border border-teal-300 bg-zinc-400 p-3 px-4 py-2 font-bold duration-200 hover:bg-zinc-500 group-hover:-translate-x-1 group-hover:-translate-y-1 dark:bg-zinc-700 dark:hover:bg-zinc-800"
 				onClick={() => registerAction({ action: "download-resume" })}
 			>
 				Resume
-			</Link>
+			</a>
 		</div>
 	);
 };
