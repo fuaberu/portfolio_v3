ef5d4d3931ba9db64fdb88843089e0652657696f
fuaberu <86504230+fuaberu@users.noreply.github.com>
2024-03-05 10:33:14 -0300

    Fix title color

--- src/components/ui/section-title.tsx
+++ src/components/ui/section-title.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export const SectionTitle = ({ title }: Props) => {
 	return (
-		<div className="bg-gradient-to-r from-teal-200 to-blue-600 bg-clip-text text-transparent">
+		<div className="bg-gradient-to-r from-teal-400 to-teal-700 bg-clip-text text-transparent dark:from-teal-100 dark:to-teal-500">
 			<h2 id="skills" className="text-center text-3xl font-bold md:text-6xl">
 				{title}
 			</h2>
