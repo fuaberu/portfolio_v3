537ab65e535286bf35e8f3d96078be4f4395ac4b
fuaberu <86504230+fuaberu@users.noreply.github.com>
2024-08-30 08:46:28 -0300

    Update resume

--- src/components/resume-link.tsx
+++ src/components/resume-link.tsx
@@ -14,8 +14,9 @@ export const ResumeLink = ({ translations }: Props) => {
 		<div className="group relative block self-stretch">
 			<div className="absolute inset-0 -z-10 rounded-md bg-teal-300"></div>
 			<a
-				href="/resume.pdf"
+				href="https://docs.google.com/document/d/17nVO9zG1l-wJlLQa1ytyxN6gyh6gAvjJ-Jykl2TpVb0/export?format=pdf"
 				target="_blank"
+				rel="noreferrer"
 				className="flex h-full flex-col items-center justify-center rounded-md border border-teal-300 font-bold duration-200 group-hover:-translate-x-1 group-hover:-translate-y-1"
 				onClick={() => registerAction({ action: "download-resume" })}
 			>
